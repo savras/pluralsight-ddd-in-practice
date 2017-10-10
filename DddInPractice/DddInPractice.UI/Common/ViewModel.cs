acce7d0f8560d69fee177624488c8cb97ad8cd0c
Vincent Gim <vgim@sateva.com.au>
2017-10-10 22:57:20 +0800

    DDD: Added UI from project file

--- DddInPractice/DddInPractice.UI/Common/ViewModel.cs
+++ DddInPractice/DddInPractice.UI/Common/ViewModel.cs
@@ -1,7 +1,30 @@
-﻿namespace DddInPractice.UI.Common
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+//using DddInPractice.UI.Utils;
+
+namespace DddInPractice.UI.Common
 {
-    public abstract class ViewModel
+    public abstract class ViewModel : INotifyPropertyChanged
     {
-        public virtual string Caption { get; set; }
+        //protected static readonly DialogService _dialogService = new DialogService();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool? _dialogResult;
+        public bool? DialogResult
+        {
+            get { return _dialogResult; }
+            protected set
+            {
+                _dialogResult = value;
+                Notify();
+            }
+        }
+
+        public virtual string Caption => string.Empty;
+
+        protected void Notify([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- DddInPractice/DddInPractice.UI/Properties/AssemblyInfo.cs
+++ DddInPractice/DddInPractice.UI/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DddInPractice.UI")]
+[assembly: AssemblyTitle("DddInPractice.Logic.UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DddInPractice.UI")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("DddInPractice.Logic.UI")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
